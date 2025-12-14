3f92de8f40fd659eee50d74902c7241d22292bbb
zDefcon <antholozadarequelme@gmail.com>
2025-12-14 04:02:44 -0500

    Agrega clase Empleado para la gestión de empleados
    Se crea la clase Empleado que incluye atributos como nombre, edad y salario base.
    Además, se implementan métodos para calcular el salario y mostrar la información del empleado.

---
+++ src/main/java/Clases/Empleado.java
@@ -0,0 +1,25 @@
+package Clases;
+
+public class Empleado {
+    protected String nombre;
+    protected int edad;
+    protected double salarioBase;
+
+    public Empleado(String nombre, int edad, double salarioBase) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.salarioBase = salarioBase;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public double calcularSalario(){
+        return salarioBase;
+    }
+
+    public String mostrarInfo() {
+        return "Nombre: " + nombre + ", Edad: " + edad + ", Salario Base: " + salarioBase;
+    }
+}
