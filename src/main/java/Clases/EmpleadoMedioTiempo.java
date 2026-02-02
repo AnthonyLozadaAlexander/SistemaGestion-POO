e991ee8358df0e00cf78404a8cb9de713b71c760
zDefcon <antholozadarequelme@gmail.com>
2026-02-01 20:03:51 -0500

    Agrega clase EmpleadoMedioTiempo para gestionar empleados a tiempo parcial
    Se crea la clase EmpleadoMedioTiempo que extiende de Empleado,
    incluyendo atributos para horas trabajadas y tarifa por hora.
    Esto permite una mejor gestión de los empleados que trabajan
    menos horas y se les paga por hora.

---
+++ src/main/java/Clases/EmpleadoMedioTiempo.java
@@ -0,0 +1,12 @@
+﻿package Clases;
+
+public class EmpleadoMedioTiempo extends Empleado{
+    private int horasTrabajadas;
+    private double tarifaPorHora;
+
+    public EmpleadoMedioTiempo(String nombre, int edad, double salarioBase, int horasTrabajadas, double tarifaPorHora){
+        super(nombre, edad, salarioBase);
+        this.horasTrabajadas = horasTrabajadas;
+        this.tarifaPorHora = tarifaPorHora;
+    }
+}
